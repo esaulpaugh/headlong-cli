b34321f1644e0b57e819b03bb2f02642f5187c0a
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2020-01-11 01:53:42 -0600

    Revert "refactor Serialize.toJsonElement"
    This reverts commit 7643c6cb1b1f1fd629fd5a238d630d2f7354233f.

--- src/main/java/com/esaulpaugh/headlong/cli/Serializer.java
+++ src/main/java/com/esaulpaugh/headlong/cli/Serializer.java
@@ -28,51 +28,70 @@ public class Serializer {
     }
 
     private static JsonElement toJsonElement(Object val) {
-        if(val.getClass().isArray()) {
-            JsonArray array = new JsonArray();
-            if(val instanceof boolean[]) {
-                for(boolean e : (boolean[]) val) array.add(toJsonElement(e));
-            } else if(val instanceof int[]) {
-                for(int e : (int[]) val) array.add(toJsonElement(e));
-            } else if(val instanceof long[]) {
-                for(long e : (long[]) val) array.add(toJsonElement(e));
-                return array;
-            } else if(val instanceof Object[]) {
-                for(Object e : (Object[]) val) array.add(toJsonElement(e));
-            } else {
-                throw new Error();
-            }
-            return array;
-        }
-        JsonObject object = new JsonObject();
         if(val instanceof Boolean) {
+            JsonObject object = new JsonObject();
             object.add("type", new JsonPrimitive("bool"));
             object.add("value", new JsonPrimitive(val.toString()));
+            return object;
         } else if(val instanceof Integer || val instanceof Long) {
+            JsonObject object = new JsonObject();
             object.add("type", new JsonPrimitive("number"));
             object.add("value", new JsonPrimitive(val.toString()));
+            return object;
         } else if(val instanceof BigInteger) {
+            JsonObject object = new JsonObject();
             object.add("type", new JsonPrimitive("string"));
             object.add("value", new JsonPrimitive("0x" + FastHex.encodeToString(((BigInteger) val).toByteArray())));
+            return object;
         } else if(val instanceof BigDecimal) {
+            JsonObject object = new JsonObject();
             object.add("type", new JsonPrimitive("number"));
             object.add("value", new JsonPrimitive(((BigDecimal) val).unscaledValue().toString()));
+            return object;
         } else if(val instanceof byte[]) {
+            JsonObject object = new JsonObject();
             object.add("type", new JsonPrimitive("buffer"));
             object.add("value", new JsonPrimitive("0x" + FastHex.encodeToString((byte[]) val)));
+            return object;
         } else if(val instanceof String) {
+            JsonObject object = new JsonObject();
             object.add("type", new JsonPrimitive("buffer"));
             object.add("value", new JsonPrimitive((String) val));
+            return object;
+        } else if(val instanceof boolean[]) {
+            JsonArray array = new JsonArray();
+            for(boolean e : (boolean[]) val) {
+                array.add(toJsonElement(e));
+            }
+            return array;
+        } else if(val instanceof int[]) {
+            JsonArray array = new JsonArray();
+            for(int e : (int[]) val) {
+                array.add(toJsonElement(e));
+            }
+            return array;
+        } else if(val instanceof long[]) {
+            JsonArray array = new JsonArray();
+            for(long e : (long[]) val) {
+                array.add(toJsonElement(e));
+            }
+            return array;
+        } else if(val instanceof Object[]) {
+            JsonArray array = new JsonArray();
+            for(Object e : (Object[]) val) {
+                array.add(toJsonElement(e));
+            }
+            return array;
         } else if(val instanceof Tuple) {
+            JsonObject object = new JsonObject();
             object.add("type", new JsonPrimitive("tuple"));
             JsonArray array = new JsonArray();
             for(Object e : (Tuple) val) {
                 array.add(toJsonElement(e));
             }
             object.add("value", array);
-        } else {
-            throw new Error();
+            return object;
         }
-        return object;
+        throw new Error();
     }
 }
